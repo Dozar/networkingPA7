2930986481a95ae7b4915562d26d4b75497b1020
Dozar <ddozar@gmail.com>
2016-03-22 12:12:08 -0700

    Added files via upload

---
+++ Movie.java
@@ -0,0 +1,31 @@
+
+package pa7;
+
+public class Movie {
+    
+    private String title, releaseDate, overview;
+ 
+    Movie(){
+        title = "";
+        releaseDate = "";
+        overview = "";
+    }
+    Movie(String titleInput, String releaseDateInput, String overviewInput){
+        title = titleInput;
+        releaseDate = releaseDateInput;
+        overview = overviewInput;
+    }
+    
+    String getTitle(){
+        return title;
+    }
+    String getReleaseDate(){
+        return releaseDate;
+    }
+    String getOverview(){
+        return overview;
+    }
+    public String toString() {
+        return "Site address: " + getDomain() + ", " + getIp();
+    }
+}
