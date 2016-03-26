28f8c03a2e82b17e4d66d9d7f07095aa6988bc34
Dozar <ddozar@gmail.com>
2016-03-25 18:15:46 -0700

    Movie client updates to reflect movie db functionality

--- MovieClient.java
+++ MovieClient.java
@@ -9,44 +9,39 @@ import java.net.Socket;
 
 public class MovieClient {
     
- public static void main(String[] args) {
-     
-    System.out.println("Echo Client");
-  
-    try 
-    {
-        System.out.println("Waiting for connection.....");
-        InetAddress localAddress = InetAddress.getLocalHost();
-        
-    try 
-    {
-        Socket clientSocket = new Socket(localAddress, 8000);
-        BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-    
-        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-        System.out.println("Connected to server");
-        Scanner scanner = new Scanner(System.in);
-        
-        while (true)
-        {
-        System.out.print("Enter text: ");
-        String inputLine = scanner.nextLine();
-     
-            if ("quit".equalsIgnoreCase(inputLine)) {
-                break;
-            }
-     
-        out.println(inputLine);
-        String response = br.readLine();
-        System.out.println("Server response: " + response);
-        }
-        
-    } catch (IOException ex) {
-        System.out.println(ex.getMessage());
-    }
-    
-    } catch (IOException ex) {
-        System.out.println(ex.getMessage());
-  }
- }
+	 public static void main(String[] args) {
+	 
+		 System.out.println("Movie Client");
+		 
+		 try {
+			 System.out.println("Waiting for connection.....");
+			 InetAddress localAddress = InetAddress.getLocalHost();
+			 
+			 try {
+				 Socket clientSocket = new Socket(localAddress, 6666);
+				 BufferedReader br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+				 PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+				 
+				 System.out.println("Connected to server");
+				 System.out.print("Enter movie year and number of movies:  ");
+				
+				 Scanner scanner = new Scanner(System.in);
+				 String inputLine = scanner.nextLine();
+				 out.println(inputLine);
+				 System.out.println("Server response: \n");
+				 String response = br.readLine();
+		
+				 String[] resultStrs = response.split(",");
+				
+				 for (String s : resultStrs) {
+					 System.out.println(s);
+				 }
+				 
+			 } catch (IOException ex) {
+				 System.out.println(ex.getMessage());
+			 }
+		 } catch (IOException ex) {
+			 System.out.println(ex.getMessage());
+		 }
+	 }
 }
