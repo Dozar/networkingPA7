db4d1119e17d2d9557704da2bda88cb3aa6dc361
alex-taxiera <alex.taxiera@mymail.champlain.edu>
2016-03-27 00:47:26 -0400

    Fixed white space

--- Movie.java
+++ Movie.java
@@ -2,28 +2,28 @@ package pa7;
 
 public class Movie {
 	private String title, releaseDate, overview;
-
-    Movie(){
-        title = "";
-        releaseDate = "";
-        overview = "";
-    }
-    Movie(String titleInput, String releaseDateInput, String overviewInput){
-        title = titleInput;
-        releaseDate = releaseDateInput;
-        overview = overviewInput;
-    }
-
-    String getTitle(){
-        return title;
-    }
-    String getReleaseDate(){
-        return releaseDate;
-    }
-    String getOverview(){
-        return overview;
-    }
-    public String toString() {
-        return "Title: " + getTitle() + "`Release Date: " + getReleaseDate() + "`Overview: " + getOverview() + "`";
-    }
+	
+	Movie(){
+		title = "";
+		releaseDate = "";
+		overview = "";
+	}
+	Movie(String titleInput, String releaseDateInput, String overviewInput){
+		title = titleInput;
+		releaseDate = releaseDateInput;
+		overview = overviewInput;
+	}
+	
+	String getTitle(){
+		return title;
+	}
+	String getReleaseDate(){
+		return releaseDate;
+	}
+	String getOverview(){
+		return overview;
+	}
+	public String toString() {
+		return "Title: " + getTitle() + "`Release Date: " + getReleaseDate() + "`Overview: " + getOverview() + "`";
+	}
 }
